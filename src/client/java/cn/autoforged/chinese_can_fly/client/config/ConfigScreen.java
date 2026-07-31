ab49550fe840dd764fcc1afa65e5cce389e4f44a
ChineseCanFly <modder@chinese-can-fly.local>
2026-07-31 16:19:46 +0800

    26.2: fix keyword list - use rowLeft/rowWidth

--- src/client/java/cn/autoforged/chinese_can_fly/client/config/ConfigScreen.java
+++ src/client/java/cn/autoforged/chinese_can_fly/client/config/ConfigScreen.java
@@ -215,11 +215,11 @@ public class ConfigScreen extends Screen {
 
 		@Override
 		public void extractContent(GuiGraphicsExtractor graphics, int x, int y, boolean hovered, float delta) {
-			int w = owner.getRowWidth();
+			int rowLeft = owner.getRowLeft(); int rowWidth = owner.getRowWidth();
 			int cy = y + (WIDGET_HEIGHT - font.lineHeight) / 2 + font.lineHeight;
-			graphics.text(font, keyword, x + 4, cy, 0xFFFFFFFF, false);
+			graphics.text(font, keyword, rowLeft + 4, cy, 0xFFFFFFFF, false);
 			String removeLabel = "[" + Component.translatable("screen." + ExampleMod.MOD_ID + ".config.remove").getString() + "]";
-			graphics.text(font, removeLabel, x + w - font.width(removeLabel) - 4, cy, 0xFFFF5555, false);
+			graphics.text(font, removeLabel, rowLeft + rowWidth - font.width(removeLabel) - 4, cy, 0xFFFF5555, false);
 		}
 
 		public boolean mouseClicked(double mouseX, double mouseY, int button) {
