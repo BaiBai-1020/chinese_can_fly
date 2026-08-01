407b683261f5b42e61d072a3ad970967a721dc12
ChineseCanFly <modder@chinese-can-fly.local>
2026-08-01 12:43:48 +0800

    26.2: Button widgets for keywords

--- src/client/java/cn/autoforged/chinese_can_fly/client/config/ConfigScreen.java
+++ src/client/java/cn/autoforged/chinese_can_fly/client/config/ConfigScreen.java
@@ -161,8 +161,7 @@ public class ConfigScreen extends Screen {
 	public void extractRenderState(GuiGraphicsExtractor graphics, int mouseX, int mouseY, float delta) {
 		super.extractRenderState(graphics, mouseX, mouseY, delta);
 		String titleStr = this.title.getString();
-		int tw = this.font.width(titleStr);
-		graphics.text(this.font, titleStr, this.width / 2 - tw / 2, 14, 0xFFFFFFFF, false);
+		graphics.drawCenteredString(this.font, titleStr, this.width / 2, 14, 0xFFFFFFFF);
 		int labelColor = 0xFFA0A0A0;
 		final int lh = this.font.lineHeight;
 		drawLabel(graphics, "screen." + ExampleMod.MOD_ID + ".config.sound_id", soundIdField, labelColor, lh);
